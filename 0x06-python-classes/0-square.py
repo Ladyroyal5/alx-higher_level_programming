aaa4939146c08d27838de7f7ea9db0508dc20912
lilian <ladyroyal.ellalily@gmail.com>
2023-01-25 00:31:50 -0800

    commit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# 0-square.py by Ehoneah Obed
-"""Defines a square """
+
+""" this module defines a empty class square """
 
 
 class Square:
     """Does nothing"""
-    passs
+    pass
